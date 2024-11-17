8aaed1585d799977b7b89350460d4979fa933bd9
Ioannis Giakisikloglou <115738506+giannisgks@users.noreply.github.com>
2024-11-17 16:51:55 +0200

    Add files via upload

--- transition.js
+++ transition.js
@@ -2,24 +2,20 @@ document.addEventListener('DOMContentLoaded', function () {
     const transitionLayer = document.querySelector('.transition-layer');
     const aboutButton = document.querySelector('.about-button');
 
-    // Handle page load transition
     setTimeout(function () {
-        transitionLayer.classList.add('loaded'); // Fade out on page load
-    }, 100); // Slight delay before starting the effect
+        transitionLayer.classList.add('loaded');
+    }, 100);
 
-    // Handle link click transition
     if (aboutButton) {
         aboutButton.addEventListener('click', function (event) {
-            event.preventDefault(); // Prevent immediate navigation
+            event.preventDefault();
 
-            // Add 'active' class to the transition layer for leaving transition
             transitionLayer.classList.remove('loaded');
             transitionLayer.classList.add('active');
 
-            // Wait for the transition to complete before navigating
             setTimeout(function () {
-                window.location.href = aboutButton.href; // Navigate to about.html
-            }, 500); // Duration matches the CSS transition time
+                window.location.href = aboutButton.href; 
+            }, 500);
         });
     }
 });
